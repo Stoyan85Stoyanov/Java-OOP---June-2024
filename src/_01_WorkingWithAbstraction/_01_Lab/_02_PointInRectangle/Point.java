823061fd6810b00537a8932fcb49a8685a8620ff
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-06-25 00:55:20 +0300

    Lab

---
+++ src/_01_WorkingWithAbstraction/_01_Lab/_02_PointInRectangle/Point.java
@@ -0,0 +1,19 @@
+package _01_WorkingWithAbstraction._01_Lab._02_PointInRectangle;
+
+public class Point {
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
---
+++ src/_01_WorkingWithAbstraction/_01_Lab/_02_PointInRectangle/Rectangle.java
@@ -0,0 +1,17 @@
+package _01_WorkingWithAbstraction._01_Lab._02_PointInRectangle;
+
+public class Rectangle {
+
+    private final Point a;
+    private final Point b;
+
+    public Rectangle(Point a, Point b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public boolean contains(Point p) {
+        return p.getX() >= a.getX() && p.getX() <= b.getX()
+                && p.getY() >= a.getY() && p.getY() <= b.getY();
+    }
+}

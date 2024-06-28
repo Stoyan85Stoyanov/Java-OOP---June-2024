eede5ead166e3b935e33d0f12f4dcd8cfcb5dbf0
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-06-28 23:30:09 +0300

    Lab

---
+++ src/_02_Encapsulation_Lab/_02_SalaryIncrease/Person.java
@@ -0,0 +1,44 @@
+package _02_Encapsulation_Lab._02_SalaryIncrease;
+
+public class Person {
+    private final String firstName;
+    private final String lastName;
+    private final int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s gets %f leva", getFirstName(), getLastName(), getSalary());
+    }
+
+    public void increaseSalary(double percentage) {
+        if (this.age < 30) {
+            this.setSalary(this.getSalary() + this.getSalary() * percentage / 200);
+        } else {
+            this.setSalary(this.getSalary() + this.getSalary() * percentage / 100);
+        }
+    }
+}

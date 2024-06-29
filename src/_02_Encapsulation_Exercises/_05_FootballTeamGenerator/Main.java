19e2d98ee0639c4c24c47901d776121406963eca
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-06-29 17:17:26 +0300

    Exercise

---
+++ src/_02_Encapsulation_Exercises/_05_FootballTeamGenerator/Main.java
@@ -0,0 +1,69 @@
+package _02_Encapsulation_Exercises._05_FootballTeamGenerator;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, Team> teams = new HashMap<>();
+        String input = scanner.nextLine();
+
+        while (!input.equals("END")) {
+
+        String[] command = input.trim().split(";");
+        String name = command[1];
+
+            switch (command[0]) {
+                case "Team" -> {
+
+                    try {
+                        Team team = new Team(name);
+                        teams.putIfAbsent(command[1], team);
+
+                    } catch (Exception exception) {
+                        System.out.println(exception.getMessage());
+                    }
+                }
+                case "Add" -> {
+
+                    if (!teams.containsKey(name)) {
+                        System.out.printf("Team %s does not exist.%n", command[1]);
+
+                    } else {
+                        try {
+                            teams.get(name).addPlayer(new Player(command[2]
+                                    , Integer.parseInt(command[3]), Integer.parseInt(command[4])
+                                    , Integer.parseInt(command[5]), Integer.parseInt(command[6])
+                                    , Integer.parseInt(command[7])));
+
+                        } catch (Exception exception) {
+                            System.out.println(exception.getMessage());
+                        }
+                    }
+                }
+                case "Remove" -> {
+
+                    try {
+                        teams.get(name).removePlayer(command[2]);
+
+                    } catch (Exception exception) {
+                        System.out.println(exception.getMessage());
+                    }
+                }
+                case "Rating" -> {
+
+                    if (!teams.containsKey(name)) {
+                        System.out.printf("Team %s does not exist.%n", name);
+
+                    } else {
+                        System.out.printf("%s - %.0f%n", name, (teams.get(name).getRating()));
+                    }
+                }
+            }
+            input = scanner.nextLine();
+        }
+    }
+}

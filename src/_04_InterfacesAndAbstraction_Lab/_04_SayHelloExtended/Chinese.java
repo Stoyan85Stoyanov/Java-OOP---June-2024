4b48b9db5624f934fa821e72c4a4d4ce2c0422a6
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-07-05 00:16:30 +0300

    Lab

---
+++ src/_04_InterfacesAndAbstraction_Lab/_04_SayHelloExtended/Chinese.java
@@ -0,0 +1,13 @@
+package _04_InterfacesAndAbstraction_Lab._04_SayHelloExtended;
+
+public class Chinese extends BasePerson {
+
+    public Chinese(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}

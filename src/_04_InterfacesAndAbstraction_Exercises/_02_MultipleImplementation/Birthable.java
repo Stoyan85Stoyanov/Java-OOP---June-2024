0b597851df5c1e61e1b226a2d5d51e218e06ae85
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-07-06 11:53:21 +0300

    Exercise

---
+++ src/_04_InterfacesAndAbstraction_Exercises/_02_MultipleImplementation/Birthable.java
@@ -0,0 +1,6 @@
+package _04_InterfacesAndAbstraction_Exercises._02_MultipleImplementation;
+
+public interface Birthable {
+
+    String getBirthDate();
+}
---
+++ src/_04_InterfacesAndAbstraction_Exercises/_02_MultipleImplementation/Person.java
@@ -0,0 +1,7 @@
+package _04_InterfacesAndAbstraction_Exercises._02_MultipleImplementation;
+
+public interface Person {
+
+    String getName();
+    int getAge();
+}

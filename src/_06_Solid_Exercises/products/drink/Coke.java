1abc9843256f4e4e3c3affebfbef65544af2d719
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-07-13 21:58:11 +0300

    Exercises

---
+++ src/_06_Solid_Exercises/products/drink/Coke.java
@@ -0,0 +1,28 @@
+package _06_Solid_Exercises.products.drink;
+
+public class Coke implements Drink{
+    public static final double CALORIES_PER_100_GRAMS = 44.0;
+    public static final double DENSITY = 0.6;
+
+    private final double milliliters;
+
+    public Coke(double milliliters) {
+        this.milliliters = milliliters;
+    }
+
+    public double getMilliliters() {
+        return milliliters;
+    }
+
+    @Override
+    public double getCalorie() {
+
+        double grams = getMilliliters() * Coke.DENSITY;
+        return (Coke.CALORIES_PER_100_GRAMS / 100) * grams;
+    }
+
+    @Override
+    public double getLiters() {
+        return milliliters / 1000;
+    }
+}

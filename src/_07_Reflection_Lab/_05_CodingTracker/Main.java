73d19900b760afdfd011c50c6be2ea2d26d22e3e
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-07-15 23:26:40 +0300

    Lab

---
+++ src/_07_Reflection_Lab/_05_CodingTracker/Main.java
@@ -0,0 +1,22 @@
+package _07_Reflection_Lab._05_CodingTracker;
+
+import java.lang.reflect.Method;
+
+public class Main {
+
+    @Author(name = "George")
+    public static void main(String[] args) {
+
+        printMethodsByAuthor(Main.class);
+
+    }
+    @Author(name = "Peter")
+    public static void printMethodsByAuthor(Class<?> clazz) {
+
+        for (Method method : clazz.getDeclaredMethods()) {
+             Author annotation = method.getAnnotation(Author.class);
+
+            System.out.println(annotation.name() + ": " + method.getName() + "()");
+        }
+    }
+}

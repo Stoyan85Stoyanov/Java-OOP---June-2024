21d02cf9e64d1309e739abb4651739a7324fb1e0
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-07-15 21:59:12 +0300

    Lab

---
+++ src/_07_Reflection_Lab/_02_GettersAndSetters/Main.java
@@ -0,0 +1,23 @@
+package _07_Reflection_Lab._02_GettersAndSetters;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Arrays.stream(Reflection.class.getDeclaredMethods())
+                .filter(current->current.getName().startsWith("get"))
+                .sorted(Comparator.comparing(Method::getName))
+                .forEach(getter -> System.out.printf("%s will return class %s%n"
+                        ,getter.getName(),getter.getReturnType().getName()));
+
+        Arrays.stream(Reflection.class.getDeclaredMethods())
+                .filter(current->current.getName().startsWith("set"))
+                .sorted(Comparator.comparing(Method::getName))
+                .forEach(setter -> System.out.printf("%s and will set field of class %s%n"
+                        ,setter.getName(),setter.getParameters()[0].getType().getName()));
+    }
+}

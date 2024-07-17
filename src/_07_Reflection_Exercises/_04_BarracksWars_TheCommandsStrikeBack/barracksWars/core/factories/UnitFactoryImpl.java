25403d8a567802d07761d96a0d8c38a7ee90a41c
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-07-17 20:40:10 +0300

    Exercises

---
+++ src/_07_Reflection_Exercises/_04_BarracksWars_TheCommandsStrikeBack/barracksWars/core/factories/UnitFactoryImpl.java
@@ -0,0 +1,28 @@
+package _07_Reflection_Exercises._04_BarracksWars_TheCommandsStrikeBack.barracksWars.core.factories;
+
+
+import _07_Reflection_Exercises._04_BarracksWars_TheCommandsStrikeBack.barracksWars.interfaces.Unit;
+import _07_Reflection_Exercises._04_BarracksWars_TheCommandsStrikeBack.barracksWars.interfaces.UnitFactory;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class UnitFactoryImpl implements UnitFactory {
+
+	private static final String UNITS_PACKAGE_NAME =
+			"barracksWars.models.units.";
+
+	@Override
+	public Unit createUnit(String unitType) {
+
+		try{
+			return (Unit) Class.forName(UNITS_PACKAGE_NAME+unitType).getConstructor().newInstance();
+
+		} catch (ClassNotFoundException
+				| NoSuchMethodException
+				| InstantiationException
+				| IllegalAccessException
+				| InvocationTargetException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+}

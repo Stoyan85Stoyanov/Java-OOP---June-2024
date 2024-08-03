a717a8c56e90148226de89d8386799bbdbe45ace
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-08-03 23:43:01 +0300

    Exam

---
+++ src/christmasPastryShop/entities/cocktails/BaseCocktail.java
@@ -0,0 +1,43 @@
+package christmasPastryShop.entities.cocktails;
+
+import christmasPastryShop.entities.cocktails.interfaces.Cocktail;
+
+public abstract class BaseCocktail implements Cocktail {
+
+    private String name;
+    private int size;
+    private double price;
+    private String brand;
+
+    public BaseCocktail(String name, int size, double price, String brand) {
+        this.name = name;
+        this.size = size;
+        this.price = price;
+        this.brand = brand;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+
+    @Override
+    public String getBrand() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}

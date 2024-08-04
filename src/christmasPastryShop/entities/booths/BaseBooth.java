4ab7a686b67b2e95f7cf589dbe730bef065baec1
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-08-04 23:31:10 +0300

    Exam

--- src/christmasPastryShop/entities/booths/BaseBooth.java
+++ src/christmasPastryShop/entities/booths/BaseBooth.java
@@ -1,9 +1,11 @@
 package christmasPastryShop.entities.booths;
 
+import christmasPastryShop.common.ExceptionMessages;
 import christmasPastryShop.entities.booths.interfaces.Booth;
 import christmasPastryShop.entities.cocktails.interfaces.Cocktail;
 import christmasPastryShop.entities.delicacies.interfaces.Delicacy;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public abstract class BaseBooth implements Booth {
@@ -18,42 +20,69 @@ public abstract class BaseBooth implements Booth {
 
     public BaseBooth(int boothNumber, int capacity, double pricePerPerson) {
         this.boothNumber = boothNumber;
-        this.capacity = capacity;
+        setCapacity(capacity);
+//        setNumberOfPeople(numberOfPeople);
         this.pricePerPerson = pricePerPerson;
+        this.isReserved = false;
+        this.delicacyOrders = new ArrayList<>();
+        this.cocktailOrders = new ArrayList<>();
+        this.price = 0;
+    }
+
+    public void setCapacity(int capacity) {
+        if (capacity <= 0) { // Не може да бъде по-малко от нула
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_TABLE_CAPACITY);
+        }
+        this.capacity = capacity;
+    }
+
+    public void setNumberOfPeople(int numberOfPeople) {
+        if (numberOfPeople <= 0){ // Не може да бъде по-малко или равно на 0
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_NUMBER_OF_PEOPLE);
+        }
+        this.numberOfPeople = numberOfPeople; // броя на хората, които искат кабина
     }
 
     @Override
     public int getBoothNumber() {
-        return 0;
+        return this.boothNumber;
     }
 
     @Override
     public int getCapacity() {
-        return 0;
+        return this.capacity;
     }
-
-    @Override
-    public boolean isReserved() {
-        return false;
-    }
-
     @Override
     public double getPrice() {
-        return 0;
+        return this.price;
+    }
+    @Override
+    public boolean isReserved() {
+        return this.isReserved;
     }
 
     @Override
-    public void reserve(int numberOfPeople) {
-
+    public void reserve(int numberOfPeople) { //Резервирайте щанда с дадения брой хора и изчислете цената на щанда
+        this.setNumberOfPeople(numberOfPeople);
+        isReserved = true;
+        this.price = this.pricePerPerson * numberOfPeople;
     }
 
     @Override
-    public double getBill() {
-        return 0;
+    public double getBill() { // Връща сметката за сепарето, всички поръчани коктейли и деликатеси
+        double bill = 0;
+        bill += cocktailOrders.stream().mapToDouble(Cocktail::getPrice).sum();
+        bill += delicacyOrders.stream().mapToDouble(Delicacy::getPrice).sum();
+        bill += getPrice();
+        return bill;
     }
 
     @Override
-    public void clear() {
-
+    public void clear() { // Премахва всички поръчани коктейли и деликатеси и накрая освобождава сепарето, задава броя на хората и цената на 0
+        this.isReserved = false;
+        this.cocktailOrders.clear();
+        this.delicacyOrders.clear();
+        this.numberOfPeople = 0;
+        this.price = 0;
     }
 }

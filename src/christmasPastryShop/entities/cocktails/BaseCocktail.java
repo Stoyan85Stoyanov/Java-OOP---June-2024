ad6e759ce14f46ddc85cb2675c0e93b4d9dbe379
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-08-04 23:31:33 +0300

    Exam

--- src/christmasPastryShop/entities/cocktails/BaseCocktail.java
+++ src/christmasPastryShop/entities/cocktails/BaseCocktail.java
@@ -1,5 +1,6 @@
 package christmasPastryShop.entities.cocktails;
 
+import christmasPastryShop.common.ExceptionMessages;
 import christmasPastryShop.entities.cocktails.interfaces.Cocktail;
 
 public abstract class BaseCocktail implements Cocktail {
@@ -10,34 +11,63 @@ public abstract class BaseCocktail implements Cocktail {
     private String brand;
 
     public BaseCocktail(String name, int size, double price, String brand) {
+        setName(name);
+        setSize(size);
+        setPrice(price);
+        setBrand(brand);
+    }
+
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) { // Ако името е null или празно място
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_NAME);
+        }
         this.name = name;
+    }
+
+    public void setSize(int size) { // Ако размерът е по-малък или равен на 0
+        if (size <= 0) {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_SIZE);
+        }
         this.size = size;
+    }
+
+    public void setPrice(double price) { // Ако цената е по-малка или равна на 0
+        if (price <= 0) {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_PRICE);
+        }
         this.price = price;
+    }
+
+    public void setBrand(String brand) {
+        if (name == null || name.trim().isEmpty()) { // Ако името е null или празно място
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_BRAND);
+        }
         this.brand = brand;
     }
 
     @Override
+    public String toString() {
+        return String.format("%s %s - %dml - %.2flv", this.name, this.brand, this.size, this.price);
+    }
+    @Override
     public String getName() {
-        return null;
+        return name;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
     }
 
     @Override
     public double getPrice() {
-        return 0;
+        return price;
     }
 
     @Override
     public String getBrand() {
-        return null;
+        return brand;
     }
 
-    @Override
-    public String toString() {
-        return super.toString();
-    }
+
 }
